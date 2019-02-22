1040ade6cbacd4e66c455852910d86fd6e85e68b
Danilo Baledio <d_baledio@u.pacific.edu>
2019-02-21 19:08:15 -0800

    Added call stack back end variable

--- src/starter/Level.java
+++ src/starter/Level.java
@@ -1,13 +1,11 @@
 package starter;
-import java.awt.Color;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-
-import javax.swing.Timer;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import acm.graphics.*;
+import java.awt.event.KeyEvent;
+
+import javax.swing.Timer;
+
 import game.CollisionChecker;
 import game.Payload;
 import game.Player;
@@ -20,141 +18,149 @@ import game.Portal;
 public class Level extends GraphicsPane implements ActionListener {
 
 	/* CONSTANTS */
-	private MainApplication program; //use this 'program.something' for all program calls
-	
+	private MainApplication program; // use this 'program.something' for all program calls
+
 	public static final int TIMER = 100;
 
-
-	
 	/* Variables */
-	
-	
+	private int callStack;
+
 	// pointers to the level before and after this level.
-	Level prev,next; 
-	
+	Level prev, next;
+
 	Portal portalLeft, portalRight;
-	Player player; 
+	Player player;
 	Payload payload;
 	private Timer timer;
 	private boolean payloadRetrieved;
 
 	// creates a new level. Differentiates between first and last level
 	// based on the string provided
-	public Level(MainApplication app, String levelType) {
+	public Level(MainApplication app, String levelType, int stack) {
 		super();
 		program = app;
 		player = new Player();
 		payload = new Payload();
 		prev = null;
 		next = null;
+		this.callStack = stack;
 		if (levelType.equals("first")) {
-			portalRight = new Portal("right"); 
-			portalLeft = null; 
+			portalRight = new Portal("right");
+			portalLeft = null;
 		} else if (levelType.equals("last")) {
-			portalRight = null; 
-			portalLeft = new Portal("left"); 
+			portalRight = null;
+			portalLeft = new Portal("left");
 		} else {
 			portalRight = new Portal("right"); // create rightmost portal
 			portalLeft = new Portal("left"); // create leftmost portal
 		}
-	
+
 		timer = new Timer(TIMER, this);
 
 		payloadRetrieved = false;
-		//movement.start();
+		// movement.start();
 	}
-	
+
 	// sets the previous level pointer
 	public void setPrev(Level previous) {
 		this.prev = previous;
 	}
-	
+
 	// sets the next level pointer
 	public void setNext(Level next) {
 		this.next = next;
 	}
-	
+
 	/**********************
 	 * KEY AND MOUSE LISTENER METHODS
+	 * 
 	 * @param e
 	 **********************/
 	@Override
 	public void keyPressed(KeyEvent e) {
-		//Key Press Left or 'A'
-		if(e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT) {
+		// Key Press Left or 'A'
+		if (e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT) {
 			player.move(-1, 0);
 		}
-		//Key Press Right or 'D'
-		if(e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) {
+		// Key Press Right or 'D'
+		if (e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			player.move(1, 0);
 		}
-		//Key Press Up or 'W'
-		if(e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP) {
+		// Key Press Up or 'W'
+		if (e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP) {
 			player.move(0, -1);
 		}
-		//Key Press Down or 'S'
-		if(e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN) {
+		// Key Press Down or 'S'
+		if (e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN) {
 			player.move(0, 1);
 		}
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		CollisionChecker.collisions(this, player,portalLeft, portalRight, payload,payloadRetrieved);
-		
+		CollisionChecker.collisions(this, player, portalLeft, portalRight, payload, payloadRetrieved);
+
 	}
-	
+
 	// start the internal timer
 	public void startTimer() {
 		timer.start();
 	}
-	
+
 	/**********************
 	 * WINDOW SWITCHING AND HIDING
 	 **********************/
 	@Override
 	public void showContents() {
-		
+
 		program.add(player.getImage());
-		
+
 		if (portalLeft != null) {
 			program.add(portalLeft.getImage());
 		}
 		if (portalRight != null) {
-		program.add(portalRight.getImage());
+			program.add(portalRight.getImage());
 		}
 		if (!(payloadRetrieved)) {
 			program.add(payload.getImage());
 		}
 	}
+
 	@Override
 	public void hideContents() {
 		program.removeAll();
 	}
-	
-	/* switches to the next level. If movingRight is true, switch to the next level.
-	 * Otherwise switch to the previous level.  */
+
+	/*
+	 * switches to the next level. If movingRight is true, switch to the next level.
+	 * Otherwise switch to the previous level.
+	 */
 	public void switchScreen(boolean movingRight) {
 		// stop the timer for this level
 		timer.stop();
 		// switch screens
 		program.switchLevel(movingRight);
 	}
-	
+
 	// removes the payload and sets payloadRetrieved to true
 	public void removePayload() {
 		player.addPayload(payload);
-		//this is a comment because I broke git
+		// this is a comment because I broke git
 		payloadRetrieved = true;
 	}
-	
+
 	// return the next level
 	public Level getNext() {
 		return next;
 	}
-	
-	// check if payload has been retrieved
+
+	// check if pay load has been retrieved
 	public boolean payloadRetrieved() {
 		return payloadRetrieved;
+	}
+
+	// returns the value in call stack
+	public int getCallStack() {
+		return this.callStack;
 	}
 }
