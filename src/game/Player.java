ef711f39c4a5d61946ed6712d71f8b543a2839df
Bette <e_orr@u.pacific.edu>
2019-02-21 18:54:31 -0800

    Attempting to implement payload following by direction

--- src/game/Player.java
+++ src/game/Player.java
@@ -3,12 +3,13 @@ package game;
 import java.util.*;
 import acm.graphics.*;
 import starter.MainApplication;
+import utility.CollisionHandler;
 
 public class Player {
 	public static final int PLAYER_SIZE = 50;
 	public static final int PLAYER_START_W = 20;
 	public static final int VELOCITY = 7;
-	public static final int TAILING_GAP = 15;
+	public static final double TAILING_GAP = 15;
 	
 	private ArrayList<Payload> collectedPayload;
 	private GOval image;
@@ -28,12 +29,22 @@ public class Player {
 	// Moves GImage
 	public void move(int x, int y) {
 		this.image.move(x * VELOCITY, y * VELOCITY);
-		double lastX = image.getX() + image.getWidth() + TAILING_GAP;
-		double lastY = image.getY();
+		double angle;
+		if (x == 1) {
+			angle = 180;
+		} else if (x == -1) {
+			angle = 0;
+		} else if (y == 1) {
+			angle = 270;
+		} else {
+			angle = 90;
+		}
+		double lastX = CollisionHandler.getCenter(image).getX() + Math.cos(angle) * (image.getWidth() / 2 + TAILING_GAP);
+		double lastY = CollisionHandler.getCenter(image).getY() + Math.sin(angle) * (image.getHeight() / 2 + TAILING_GAP);
 		for (Payload load: collectedPayload) {
-			load.moveTo(lastX, lastY);
-			lastX = load.getImage().getX() + load.getImage().getWidth() + TAILING_GAP;
-			lastY = load.getImage().getY();
+			load.moveTo(lastX - load.getImage().getWidth() / 2, lastY - load.getImage().getHeight() / 2);
+			lastX = CollisionHandler.getCenter(load.getImage()).getX() + Math.cos(angle) * (load.getImage().getWidth() / 2 + TAILING_GAP);
+			lastY = CollisionHandler.getCenter(load.getImage()).getY() + Math.sin(angle) * (load.getImage().getHeight() / 2 + TAILING_GAP);
 		}
 	}
 
