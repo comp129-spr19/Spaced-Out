1396bcd4e47b30422f32898928db9c09f0809689
Danilo Baledio <d_baledio@u.pacific.edu>
2019-03-07 16:02:30 -0800

    Our project is now a Gradle project. TextClass for unit testing has been added.

---
+++ src/TestClass.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestClass {
+
+	@Test
+	public void testRobotCoordinates() {
+		System.out.println("I hope 2 + 2 works.");
+		assertTrue(2 + 2 == 4);
+	}
+
+}
